348df3666a72aa9ab0f1f7557385d28594160d3b
taitu <shigaibaev.a@gmail.com>
2017-10-08 18:06:12 +0600

    Docker compose

--- admin/src/components/products/ProductCreate.jsx
+++ admin/src/components/products/ProductCreate.jsx
@@ -8,69 +8,69 @@ import Product from './Product';
 import Uploader from './Uploader';
 
 class ProductCreate extends React.Component {
-  static PropTypes = {
-    push: PropTypes.func.isRequired,
-    createProductAction: PropTypes.func.isRequired,
-    token: PropTypes.string.isRequired,
-    fetching: PropTypes.bool.isRequired,
-    successMessage: PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.onSubmitProduct = this.onSubmitProduct.bind(this);
-
-    this.state = {
-      product: {
-        title: null,
-        role: null,
-        code: null,
-        quantity: null,
-        created_on: null,
-        expires_on: null,
-        weight: null,
-        volume: null,
-        price: null,
-        user: null,
-      },
-      showUploader: false
+    static PropTypes = {
+        push: PropTypes.func.isRequired,
+        createProductAction: PropTypes.func.isRequired,
+        token: PropTypes.string.isRequired,
+        fetching: PropTypes.bool.isRequired,
+        successMessage: PropTypes.string,
     };
-  }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.successMessage) {
-      //this.props.push('/products');
-      this.setState({
-        showUploader: true
-      });
+    constructor(props) {
+        super(props);
+
+        this.onSubmitProduct = this.onSubmitProduct.bind(this);
+
+        this.state = {
+            product: {
+                title: null,
+                role: null,
+                code: null,
+                quantity: null,
+                created_on: null,
+                expires_on: null,
+                weight: null,
+                volume: null,
+                price: null,
+                user: null,
+            },
+            showUploader: false
+        };
     }
-  }
 
-  onSubmitProduct(data) {
-    this.props.createProductAction(data, this.props.token)
-      .then(product => this.setState({ product }));
-  }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.successMessage) {
+            //this.props.push('/products');
+            this.setState({
+                showUploader: true
+            });
+        }
+    }
 
-  render() {
-    const { product, showUploader } = this.state;
+    onSubmitProduct(data) {
+        this.props.createProductAction(data, this.props.token)
+            .then(product => this.setState({ product }));
+    }
 
-    return (
-      <div className='main product'>
-        <Dimmer active={this.props.fetching}>
-          <Loader />
-        </Dimmer>
-        <h1 className='title title--primary'>Создание товара</h1>
-        <h3 className='title title--secondary'>Здесь вы можете забить необходимые поля для описания товара</h3>
-        {!showUploader && <Product product={ product } onSubmitProduct={ this.onSubmitProduct } showUploader={showUploader} />}
-        {product.id && showUploader && <Uploader productId={product.id} />}
-      </div>
-    );
-  }
+    render() {
+        const { product, showUploader } = this.state;
+
+        return (
+            <div className='main product'>
+              <Dimmer active={this.props.fetching}>
+                <Loader />
+              </Dimmer>
+              <h1 className='title title--primary'>Создание товара</h1>
+              <h3 className='title title--secondary'>Здесь вы можете забить необходимые поля для описания товара</h3>
+                {!showUploader && <Product product={ product } onSubmitProduct={ this.onSubmitProduct } showUploader={showUploader} />}
+                {product.id && showUploader && <Uploader productId={product.id} />}
+            </div>
+        );
+    }
 }
 
 export default connect((state) => ({
-  token: state.auth.token,
-  successMessage: state.products.get('successMessage'),
-  fetching: state.products.get('fetching'),
+    token: state.auth.token,
+    successMessage: state.products.get('successMessage'),
+    fetching: state.products.get('fetching'),
 }), { push, createProductAction, })(ProductCreate);
